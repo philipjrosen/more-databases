beb49dae397071de6c919780251f921befe278da
Jono Xia <jono@fastmail.fm>
2013-01-08 13:28:35 -0800

    Improved the MongoArchive jasmine spec.

--- spec/MongoArchiveSpec.js
+++ spec/MongoArchiveSpec.js
@@ -1,24 +1,24 @@
 /* You'll need to have MongoDB and your Node archive server
  * running for these tests to pass. */
 
-var mongodb = require('mongodb');
+var mongodb = require("mongodb");
 var request = require("request");
 
 describe("Persistent Node Chat Server", function() {
   var mongoServer = new mongodb.Server("127.0.0.1", 27017, {});
-  // TODO edit this line if your database name is not "test":
-  var mongoClient = new mongodb.Db('test', server);
+  // TODO edit this line if your database name is not "archive":
+  var mongoClient = new mongodb.Db("archive", server);
 
   it("Should store requested documents in Mongo", function(done) {
 
     /* TODO edit these variables to match the interface of your
      * archive server. */
-    var archiveUri = "http://127.0.0.1:8080/";
-    var form = {page: "example.com"};
+    var archiveServer = "http://127.0.0.1:8080/index.html";
+    var archiveForm = {url: "google.com"};
 
     // Post a message to the archive server:
     request({method: "POST",
-             uri: archiveUri,
+             uri: archiveServer,
              form: archiveForm
             },
             function(error, response, body) {
@@ -33,9 +33,13 @@ describe("Persistent Node Chat Server", function() {
                   collection.find().toArray(function(err, results) {
                     // Should have one result:
                     expect(results.length).toEqual(1);
-                    /* TODO write a test using expect() that tests
-                     * whether the Mongo document contains the expected
-                     * HTML for the requsted web page. */
+
+                    // TODO edit this test to match the name of the
+                    // property where you're storing the page source:
+                    expect(results[0].pageSource.indexOf(
+                      "<title>Google</title>")
+                          ).toBeGreaterThan(0);
+
                     done();
                   });
                 });
