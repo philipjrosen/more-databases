09c0b2750316645bb6e8cb6256b6e25167042f8a
Jono Xia <jono@fastmail.fm>
2013-01-08 10:42:15 -0800

    Switched from NodeORM to Sequelize. Wrote the sample code demonstrating how to use Sequelize.

--- orm-example.js
+++ orm-example.js
@@ -1,43 +1,41 @@
-// TODO get this from https://github.com/dresende/node-orm2
-// npm install orm@2.0.0-alpha6
+/* You'll need to 
+ * npm install sequelize
+ * before running this example. Documentation is at http://sequelizejs.com/ 
+ */
 
-var orm = require('orm');
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("chat", "root", "");
+/* TODO this constructor takes the database name, username, then password.
+ * Modify the arguments if you need to */
 
-orm.connect("mysql://username:password@host/database", function (err, db) {
-    if (err) throw err;
-
-    var Person = db.define('person', {
-        name      : String,
-        surname   : String,
-        age       : Number,
-        male      : Boolean,
-        continent : [ 'Europe', 'America', 'Asia', 'Africa', 'Australia', 'Antartica' ], // ENUM type
-        photo     : Buffer, // BLOB/BINARY
-        data      : Object // JSON encoded
-    }, {
-        methods: {
-            fullName: function () {
-                return this.name + ' ' + this.surname;
-            }
-        },
-        validations: {
-            age: orm.validators.rangeNumber(18, undefined, 'under-age')
-        }
-    });
-
-    Person.find({ surname: "Doe" }, function (err, people) {
-        // SQL: "SELECT * FROM person WHERE surname = 'Doe'"
-
-        console.log("People found: %d", people.length);
-        console.log("First person: %s, age %d", people[0].fullName(), people[0].age);
-
-        people[0].age = 16;
-        people[0].save(function (err) {
-            // err.msg = 'under-age';
-        });
-    });
+/* first define the data structure by giving property names and datatypes
+ * See http://sequelizejs.com for other datatypes you can use besides STRING. */
+var User = sequelize.define('User', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING,
+  joined: Sequelize.DATE
 });
 
+/* .sync() makes Sequelize create the database table for us if it doesn't
+ *  exist already: */
+User.sync().success(function() {
+  /* This callback function is called once sync succeeds. */
 
-// TODO how do we create a new entity with this ORM???
-// Apparently it's with new Model(). Try that!
+  // now instantiate an object and save it:
+  var newUser = User.build({username: "Jean Valjean",
+                            password: "24601",
+                            joined: Date.now()
+                           });
+  newUser.save().success(function() {
+
+    /* This callback function is called once saving succeeds. */
+
+    // Retrieve objects from the database:
+    User.findAll({ where: {username: "Jean Valjean"} }).success(function(usrs) {
+      // This function is called back with an array of matches.
+      for (var i = 0; i < usrs.length; i++) {
+        console.log(usrs[i].username + " has password " + usrs[i].password);
+      }
+    });
+  });
+});
